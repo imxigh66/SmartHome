e0180f43108e61cd1ea224b9b756a7411e32ecd6
imxigh66 <vladakasapcuk@gmail.com>
2026-05-24 20:39:47 +0300

    enhance tariff management
    Added `TariffController` with endpoints for retrieving and updating user tariffs.
    Introduced `UpdateTariffCommand` and `GetMyTariffQuery` records for data encapsulation.
    Implemented `UpdateTariffCommandHandler` and `GetMyTariffQueryHandler` for command and query processing.
    Added `GetGlobalTariffs` method for unauthenticated access to a static list of tariffs.
    These changes improve the application's tariff management capabilities.

---
+++ SmartHome.Application/Tariffs/Commands/UpdateTariffCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SmartHome.Application.Tariffs.Queries;
+
+namespace SmartHome.Application.Tariffs.Commands
+{
+    public record UpdateTariffCommand(
+    Guid UserId,
+    string Provider,
+    string PlanType,
+    decimal SingleRate,
+    decimal DayRate,
+    decimal NightRate
+) : IRequest<TariffResult>;
+}
